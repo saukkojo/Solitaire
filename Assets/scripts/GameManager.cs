bf7f233683b2aed245341ddb71a1c064fecb47f2
saukkojo <johanna.k.saukkonen@tuni.fi>
2023-06-28 17:21:34 +0300

    some ui buttons and restart

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -89,5 +90,10 @@ public class GameManager : MonoBehaviour
         {
             Debug.Log("Draw deck error");
         }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
